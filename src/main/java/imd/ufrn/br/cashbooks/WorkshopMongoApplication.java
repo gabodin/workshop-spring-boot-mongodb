d97888d5ff2add72dc6a85bba0877230c138e2b8
Victor Castro <vgabriel2211@gmail.com>
2021-09-02 11:53:59 -0300

    Projeto criado

---
+++ src/main/java/imd/ufrn/br/cashbooks/WorkshopMongoApplication.java
@@ -0,0 +1,13 @@
+package imd.ufrn.br.cashbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/imd/ufrn/br/cashbooks/WorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package imd.ufrn.br.cashbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
