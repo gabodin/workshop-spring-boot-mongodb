c14ea250decfba5b1c665666ee04df59c827c046
Victor Castro <vgabriel2211@gmail.com>
2021-09-02 12:29:43 -0300

    Entity User e REST funcionando

--- src/main/java/imd/ufrn/br/workshopmongo/WorkshopMongoApplication.java
+++ src/main/java/imd/ufrn/br/workshopmongo/WorkshopMongoApplication.java
@@ -1,4 +1,4 @@
-package imd.ufrn.br.cashbooks;
+package imd.ufrn.br.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
